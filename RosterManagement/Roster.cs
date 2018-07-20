b8927e54d9d9ef14e5a38371b086a02f211c3732
Rishabh Agrawal <rishabh120296@gmail.com>
2018-07-20 11:25:10 +0530

    Implemented Roster Management using collections

--- RosterManagement/Roster.cs
+++ RosterManagement/Roster.cs
@@ -20,6 +20,11 @@ namespace RosterManagement
         /// <param name="wave">Refers to the Wave number</param>
         public void Add(string cadet, int wave)
         {
+            List<string> wave_cadet = new List<string>();
+            wave_cadet = Grade(wave);
+            wave_cadet.Add(cadet);
+            _roster[wave] = wave_cadet;
+
         }
 
         /// <summary>
@@ -30,6 +35,11 @@ namespace RosterManagement
         public List<string> Grade(int wave)
         {
             var list = new List<string>();
+            if (_roster.ContainsKey(wave))
+            {
+                list = _roster[wave];
+            }
+            list.Sort();
             return list;
         }
 
@@ -40,6 +50,21 @@ namespace RosterManagement
         public List<string> Roster()
         {
             var cadets = new List<string>();
+            var keys = new List<int>();
+            foreach(int key in _roster.Keys)
+            {
+                keys.Add(key);
+            }
+            keys.Sort();
+            foreach(int key in keys)
+            {
+                foreach (string cadet in Grade(key))
+                {
+                    cadets.Add(cadet);
+                    cadets.OrderBy(p => p);
+                }
+                
+            }
             return cadets;
         }
     }
